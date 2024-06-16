4cff70567af0038b2f78cc67f3fa85974eaaea89
Michał Siwanowicz <michalsiwanowicz@gmail.com>
2024-06-16 13:20:36 +0200

    setup test case

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   fullyParallel: true,
-  reporter: [['html'], ['list']],
+  reporter: [["html"], ["list"]],
   use: {
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
+    baseURL: "https://platform.droplo.eu",
   },
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
   ],
 });
